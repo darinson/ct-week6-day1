dfc74f44f6a3a6f08ddea33e7c48a3b0ec0cda2c
Darin Son <dson64@outlook.com>
2021-04-19 15:39:08 -0700

    completed exercises

--- js/main.js
+++ js/main.js
@@ -6,21 +6,44 @@ the list and checks that the current name is in the string passed in. The output
 let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
 let dog_names = ["Max", "HAS", "PuRple", "dog"]
 
-function findWords() {
-    //Your code goes here
+let findWords = (dstring, dlist) => {
+    let st = dstring.toLowerCase().split(/[ ,]+/)
+    let na = dlist.map(i => i.toLowerCase())
+    let matches = []
+    for (let i = 0; i < na.length; i++) {
+        if (st.includes(na[i])) {
+            matches.push(na[i])
+        }
+    }
+    if (matches.length == 0) {
+        console.log('There are no matches here')
+    }
+    else {
+        console.log('We have a match at:')
+        for (let i = 0; i < matches.length; i++) {
+            console.log(matches[i])
+        }
+    }
 }
 
-//Call method here with parameters
+findWords(dog_string, dog_names)
 
 //============Exercise #2 ============//
-/*Write a fucntion that takes in an array and removes every even index with a splice,
+/*Write a function that takes in an array and removes every even index with a splice,
 and replaces it with the string "even index" */
 
 let arr = ["Max", "Baseball", "Reboot", "Goku", "Trucks", "Rodger"]
 
-function replaceEvens(arr) {
-    //code goes here
+function replaceEvens(listy) {
+    for (let i = 0; i < listy.length; i++) {
+        if (i % 2 == 0) {
+            listy.splice(i, 1, 'even index')
+        }
+    }
+    return (listy)
 }
+
+console.log(replaceEvens(arr))
 
 //Expected output
 //Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
